f0c75bd9b568adc0812a659144282761c0d958d8
Matheus Artilino <36131994+f4tjs@users.noreply.github.com>
2021-04-11 21:30:54 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,38 @@ const routes = express.Router()
 
 const profile = {
     name: "Matheus",
-    avatar: "https://avatars.githubusercontent.com/u/36131994?v=4",
+    avatar: "https://github.com/f4tjs.png",
     'monthly-budget': 3000,
     'days-per-week': 5,
     'hours-per-day': 5,
     'vacation-per-year': 4
 }
 
+const jobs = []
+
+console.log(jobs)
+
 routes.get('/', (req, res) => res.render('index'))
 routes.get('/job', (req, res) => res.render('job'))
+routes.post('/job', (req, res) => {
+    
+    const lastID = jobs[jobs.length - 1]?.id || 1
+
+    jobs.push({
+        id: lastID + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now()
+    })
+
+    console.log(jobs)
+
+    return res.status(200).redirect('/')
+})
 routes.get('/job/edit', (req, res) => res.render('job-edit'))
 routes.get('/profile', (req, res) => res.render('profile', {
     profile
 }))
-
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ server.set('views', path.join(__dirname, './views'));
 
 server.use(express.static('public'))
 
+server.use(express.urlencoded({extended: true}))
 server.use(routes)
 
-server.listen(3000, () => console.log('RODANDO'))
+const port = 3000;
+
+server.listen(port, () => console.log(`SERVER RUNNING ON PORT: ${port}`))
